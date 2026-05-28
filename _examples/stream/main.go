1b85496712ad7cf04e61a035e631e881e2c707cf
Angus-Warman <anguswarman@gmail.com>
2026-05-28 16:15:39 +0800

    SSE response

---
+++ _examples/stream/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Angus-Warman/httpmin"
+	"github.com/Angus-Warman/httpmin/response"
+)
+
+func timeStream(r *http.Request, notify func(string)) error {
+	for {
+		err := r.Context().Err()
+
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		now := time.Now()
+
+		log.Println(now.String())
+		notify(now.String())
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	httpmin.Setup().RouteHandler("/time", response.Stream(timeStream)).Run()
+}
---
+++ response/stream.go
@@ -0,0 +1,37 @@
+package response
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Turns a long-running operation into a server-sent event stream
+func Stream(operation func(r *http.Request, notify func(event string)) error) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		flusher, ok := w.(http.Flusher)
+
+		if !ok {
+			http.Error(w, "streaming unsupported", http.StatusInternalServerError)
+			return
+		}
+
+		// SSE headers
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+
+		// nginx
+		w.Header().Set("X-Accel-Buffering", "no")
+
+		notify := func(event string) {
+			fmt.Fprintf(w, "data: %s\n\n", event)
+			flusher.Flush()
+		}
+
+		err := operation(r, notify)
+
+		if err != nil {
+			notify(err.Error())
+		}
+	})
+}
