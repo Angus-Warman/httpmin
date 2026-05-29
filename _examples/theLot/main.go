b91eb4009060b38062870fd56cfd079116d35366
Angus-Warman <anguswarman@gmail.com>
2026-05-29 12:40:29 +0800

    _examples/theLot

---
+++ _examples/theLot/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Angus-Warman/httpmin"
+	"github.com/Angus-Warman/httpmin/middleware"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
+func myCustomMiddleware() func(http.Handler) http.Handler {
+	f := func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			// Do something here
+			next.ServeHTTP(w, r)
+		}
+		return http.HandlerFunc(fn)
+	}
+
+	return f
+}
+
+func main() {
+	c := httpmin.Setup() // Modifications can be chained
+
+	c.OnPort("8081") // Port used comes from: env variables, .env file, this function, "8080" (in that order)
+	c.Route("/hello", helloWorld)
+	c.ServeFolder("public") // Not embedded, add any file to folder and load the page
+	c.Use(middleware.Cors())
+	c.Use(myCustomMiddleware())
+	c.PublicIP() // Listen on "0.0.0.0"
+	c.UseSelfSignedHTTPS()
+
+	c.Run()
+}
